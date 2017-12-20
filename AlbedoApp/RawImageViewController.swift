12229cc8a35bfe1d8f7b5f7a1a32f1193db2d890
Camden Tatsapaugh <crt2004@wildcats.unh.edu>
2017-12-19 20:47:05 -0500

    Added required request for authorization for camera - user must agree to allow the app to access the camera on the first time (per Apple requirement)

--- AlbedoApp/RawImageViewController.swift
+++ AlbedoApp/RawImageViewController.swift
@@ -4,21 +4,50 @@
 //
 //  Created by crt2004 on 12/19/17.
 //
-//
 
 import UIKit
+import AVFoundation // needed for functions related to the camera
 
 class RawImageViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("If this appears in the console then the segue happened")
+        self.checkCameraAuthorization { authorized in
+            if authorized {
+                // Proceed to set up and use the camera.
+            } else {
+                print("Permission to use camera denied. Camera permission must be granted in order to use Albedo App.")
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // checks to see if camera usage has been authorized and prompts user if it is not
+    func checkCameraAuthorization(_ completionHandler: @escaping ((_ authorized: Bool) -> Void)) {
+        switch AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo) {
+        case .authorized:
+            //The user has previously granted access to the camera.
+            completionHandler(true)
+            
+        case .notDetermined:
+            // The user has not yet been presented with the option to grant video access so request access.
+            AVCaptureDevice.requestAccess(forMediaType: AVMediaTypeVideo, completionHandler: { success in
+                completionHandler(success)
+            })
+            
+        case .denied:
+            // The user has previously denied access.
+            completionHandler(false)
+            
+        case .restricted:
+            // The user doesn't have the authority to request access e.g. parental restriction.
+            completionHandler(false)
+        }
     }
 }
 
