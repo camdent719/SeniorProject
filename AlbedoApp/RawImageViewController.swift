3040b4cafed58abd38baec9cca99dba9f4b0c919
Camden Tatsapaugh <crt2004@wildcats.unh.edu>
2017-12-19 20:12:28 -0500

    Segue to a second view controller works when the button is tapped/clicked

--- AlbedoApp/RawImageViewController.swift
+++ AlbedoApp/RawImageViewController.swift
@@ -1,21 +1,21 @@
 //
-//  RootViewController.swift
+//  RawImageViewController.swift
 //  AlbedoApp
 //
-//  Created by crt2004 on 12/18/17.
+//  Created by crt2004 on 12/19/17.
 //
 //
 
 import UIKit
 
-class RootViewController: UIViewController {
-
+class RawImageViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-      
-        print("If this appears in the console then it ran successfully")
+        
+        print("If this appears in the console then the segue happened")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
