de40e442fdc6750af49171dfd44491bf5edd9fb5
AlbedoDev <albedodev@hutt.sr.unh.edu>
2018-02-19 10:53:58 -0500

    Added code to get user's GPS location - asks for permission on first time

--- AlbedoApp/RootViewController.swift
+++ AlbedoApp/RootViewController.swift
@@ -4,19 +4,35 @@
 //
 //  Created by crt2004 on 12/18/17.
 //
-//
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class RootViewController: UIViewController {
+class RootViewController: UIViewController, CLLocationManagerDelegate {
 
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.locationManager.requestWhenInUseAuthorization() // user must agree to use location (first time only)
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        print("Current location: \(location.latitude), \(location.longitude)")
     }
 }
 
