fc9a7977084e6e9b93a91141d4611b3a099dd9b9
AlbedoDev <albedodev@hutt.sr.unh.edu>
2018-03-21 11:05:11 -0400

    Still working on calculation

--- AlbedoApp/PhotoData.swift
+++ AlbedoApp/PhotoData.swift
@@ -24,7 +24,7 @@ struct PhotoData {
         }
         let rgbDown: [UInt32] = PhotoData.sum(pixelBuffer: pixelBufferDown)
         
-        guard let pixelBufferUp = CMSampleBufferGetImageBuffer(PhotoData.rawPhotos[1]) else {
+        /*guard let pixelBufferUp = CMSampleBufferGetImageBuffer(PhotoData.rawPhotos[1]) else {
             print("rawSampleBuffer does not contain a CVPixelBuffer")
             return
         }
@@ -35,43 +35,43 @@ struct PhotoData {
         let albedoB = rgbDown[2] / rgbUp [2]
         
         let albedo = albedoR + albedoG + albedoB
-        print("******* Albedo (does this number make any sense?! who knows: \(albedo)")
+        print("******* Albedo (does this number make any sense?! who knows: \(albedo)")*/
     }
     
     //static func sum(pixelBuffer: CVPixelBuffer) -> [UInt32] {
     static func sum(pixelBuffer: CVPixelBuffer) -> [UInt32] {
         CVPixelBufferLockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
         //let int32Buffer = unsafeBitCast(CVPixelBufferGetBaseAddress(pixelBuffer), to: UnsafeMutablePointer<UInt32>.self)
-        let bytesPerRow = CVPixelBufferGetBytesPerRow(pixelBuffer)
+        let bytesPerRow: size_t = CVPixelBufferGetBytesPerRow(pixelBuffer)
         let bufferWidth = Int(CVPixelBufferGetWidth(pixelBuffer))
         let bufferHeight = Int(CVPixelBufferGetHeight(pixelBuffer))
-        let baseAddress: UInt32 = CVPixelBufferGetBaseAddress(pixelBuffer)
+        let baseAddress = CVPixelBufferGetBaseAddress(pixelBuffer)
         
         //print("int32Buffer:\(type(of: int32Buffer))")
         print("bytesPerRow:\(bytesPerRow)")
         
         //let x = 0
         //let y = 0
-        //let width = CVPixelBufferGetWidth(pixelBufferUp)
-        //let height = CVPixelBufferGetHeight(pixelBufferUp)
+        let width = CVPixelBufferGetWidth(pixelBuffer)
+        let height = CVPixelBufferGetHeight(pixelBuffer)
         //let width = 30 //PhotoData.screenWidth
         //let height = 30//PhotoData.screenHeight
-        //print("width:\(width)")
-        //print("height:\(height)")
+        print("width:\(width)")
+        print("height:\(height)")
         
         var b: UInt32 = 0
         var g: UInt32 = 0
         var r: UInt32 = 0
-        let kBytesPerPixel: Int = 4
+        /*let kBytesPerPixel: Int = 4
         for row: Int in 0 ..< bufferHeight {
-            var pixel = Int(baseAddress) + row * bytesPerRow
+            var pixel = (baseAddress + row * bytesPerRow) as? [UInt32]//var pixel = Int(baseAddress) + row * bytesPerRow
             for col in 0 ..< bufferWidth {
                 b += pixel[0] // blue val
                 g += pixel[1] // green val
                 r += pixel[2] // red val
             }
             pixel += kBytesPerPixel
-        }
+        }*/
       
         
         /*for h in 0 ..< height {
